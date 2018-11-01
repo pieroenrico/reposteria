a51cc1a41f9964119efa552677d114c09e925cf6
Piero Bona <pierobona@Pieros-MacBook-Pro-7.local>
2018-11-01 12:20:41 -0600

    edits

---
+++ src/ReposteriaServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Pieroenrico\Reposteria;
+
+use Illuminate\Support\ServiceProvider;
+
+class ReposteriaServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
